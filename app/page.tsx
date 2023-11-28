2d11d71ffd5792040f1d92d5beaab9e21d0b63ee
Rencas <rencasdag.12@gmail.com>
2023-11-27 22:49:36 -0500

    server side-rendering + fix scroll router push

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,16 @@
 import { CustomFilter, Hero, SearchBar } from '@/components';
 import CarCard from '@/components/CarCard';
 import { fetchCars } from '@/utils';
+import { manufacturers } from '@/constants';
 
-export default async function Home() {
-  const allCars = await fetchCars();
-
-  console.log(allCars);
+export default async function Home({ searchParams }) {
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || '',
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || '',
+    limit: searchParams.limit || 10,
+    model: searchParams.model || '',
+  });
 
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
--- types/index.ts
+++ types/index.ts
@@ -39,3 +39,11 @@ export interface SearchManuFacturerProps {
   manufacturer: string;
   setManuFacturer: (manufacturer: string) => void;
 }
+
+export interface FilterProps {
+  manufacturer: string;
+  year: number;
+  fuel: string;
+  limit: number;
+  model: string;
+}
